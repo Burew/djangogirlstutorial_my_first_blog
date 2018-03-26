bbd58d681d62984f7bc231de88afa24567b11e05
Keung Huang <notinschool@gmail.com>
2018-03-25 22:54:15 -0700

    Added form to add new post and edit a post

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), #only empty strings will match
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'), 
+    url(r'^post/new/$', views.post_new, name='post_new'), #for submitting a new post
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'), 
 ]
