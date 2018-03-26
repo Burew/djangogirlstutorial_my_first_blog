258eb57918cfc2c724fbcf7fc8846098e69cb2cf
Keung Huang <notinschool@gmail.com>
2018-03-25 21:43:45 -0700

    Added post_detail view and added path for it

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,3 +8,9 @@ def post_list(request):
     
     #render the blog/post_list.html template with posts passed in as extra data
     return render(request, 'blog/post_list.html', {"posts":posts})
+
+# this view is given pk as another param from urls.py (the regex named capture group)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {"post":post})
+
